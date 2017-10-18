7d97fbedce49f8cc9b46711630051a2f1a35a8c9
csalmon- <salmonl.colin@gmail.com>
2017-10-18 16:52:24 +0200

    feat(moderation): display proposals' events' arguments

--- src/main/scala/org/make/backoffice/components/proposal/ModerationHistoryComponent.scala
+++ src/main/scala/org/make/backoffice/components/proposal/ModerationHistoryComponent.scala
@@ -4,8 +4,10 @@ import io.github.shogowada.scalajs.reactjs.React
 import io.github.shogowada.scalajs.reactjs.VirtualDOM._
 import io.github.shogowada.scalajs.reactjs.classes.ReactClass
 import io.github.shogowada.scalajs.reactjs.elements.ReactElement
+import io.github.shogowada.statictags.Element
 import org.make.backoffice.facades.MaterialUi._
-import org.make.backoffice.models.{ProposalAction, SingleProposal}
+import org.make.backoffice.helpers.Configuration
+import org.make.backoffice.models.{ProposalAction, SingleProposal, ThemeId}
 
 import scala.scalajs.js
 import scala.scalajs.js.Date
@@ -14,16 +16,30 @@ object ModerationHistoryComponent {
   case class HistoryProps(proposal: SingleProposal)
   case class HistoryState(contentActions: ReactElement)
 
-  def explicitAction(actionType: String, arguments: js.Dictionary[String]): String = {
-    val actions = Map(
-      "propose" -> "Proposal creation",
-      "accept" -> "Accepted",
-      "refuse" -> s"Refused with reason: ${arguments.getOrElse("reason", "no reason provided")}"
+  def explicitAction(actionType: String, arguments: js.Dictionary[String]): Element = {
+    val actions: Map[String, Element] = Map(
+      "propose" -> <.p()("Proposal creation"),
+      "refuse" -> <.p()(s"Refused with reason: ${arguments.getOrElse("refusalReason", "no reason provided")}"),
+      "accept" -> <.p()(
+        "Accepted with:",
+        <.ul()(
+          <.li()({
+            val theme: String =
+              arguments
+                .get("theme")
+                .map(themeId => Configuration.getThemeFromThemeId(ThemeId(themeId)))
+                .getOrElse("no theme provided")
+            s"theme: $theme"
+          }),
+          <.li()(s"tags: ${arguments.getOrElse("tags", "no tags provided")}"),
+          <.li()(s"labels: ${arguments.getOrElse("labels", "no labels provided")}")
+        )
+      )
     )
-    val defaultMessage: String = if (arguments.isEmpty) {
-      actionType
+    val defaultMessage: Element = if (arguments.isEmpty) {
+      <.p()(actionType)
     } else {
-      s"[$actionType] with arguments:${arguments.mkString(",")}"
+      <.p()(s"[$actionType] with arguments:${arguments.mkString(",")}")
     }
     actions.getOrElse(actionType, defaultMessage)
   }
@@ -34,7 +50,7 @@ object ModerationHistoryComponent {
         .map(_.sortBy(-_.date.toString.toLong).map { event =>
           <.TableRow(^.key := s"${event.date}-${event.actionType}")(
             <.TableRowColumn()(new Date(event.date.toString.toLong).toUTCString()),
-            <.TableRowColumn()(explicitAction(event.actionType, event.arguments)),
+            <.TableRowColumn()(<.p()(explicitAction(event.actionType, event.arguments))),
             <.TableRowColumn()(event.user.map(_.firstName).getOrElse("-"))
           )
         })
