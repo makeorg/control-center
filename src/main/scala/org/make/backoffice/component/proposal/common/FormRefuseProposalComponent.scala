017b90cc527fca3a737ad0a054d9cbea17864343
cpestoury <cp@make.org>
2018-05-07 10:19:49 +0200

    fix(transverse): add next proposal to postpone and refuse
    Closes #107

--- src/main/scala/org/make/backoffice/component/proposal/common/FormRefuseProposalComponent.scala
+++ src/main/scala/org/make/backoffice/component/proposal/common/FormRefuseProposalComponent.scala
@@ -3,9 +3,11 @@ package org.make.backoffice.component.proposal.common
 import io.github.shogowada.scalajs.reactjs.React
 import io.github.shogowada.scalajs.reactjs.VirtualDOM._
 import io.github.shogowada.scalajs.reactjs.classes.ReactClass
+import io.github.shogowada.scalajs.reactjs.events.SyntheticEvent
 import io.github.shogowada.scalajs.reactjs.router.RouterProps._
 import io.github.shogowada.scalajs.reactjs.router.WithRouter
 import io.github.shogowada.statictags.Element
+import org.make.backoffice.client.NotFoundHttpException
 import org.make.backoffice.facade.MaterialUi._
 import org.make.backoffice.util.Configuration
 import org.make.backoffice.model.SingleProposal
@@ -27,14 +29,12 @@ object FormRefuseProposalComponent {
 
   lazy val reactClass: ReactClass =
     WithRouter(
-      React.createClass[FormProps, FormState](
-        getInitialState = { _ =>
-          FormState(reasons = reasons)
-        },
-        componentWillReceiveProps = { (self, props) =>
-          self.setState(_.copy(isLocked = props.wrapped.isLocked))
-        },
-        render = { self =>
+      React.createClass[FormProps, FormState](getInitialState = { _ =>
+        FormState(reasons = reasons)
+      }, componentWillReceiveProps = { (self, props) =>
+        self.setState(_.copy(isLocked = props.wrapped.isLocked))
+      }, render = {
+        self =>
           def handleReasonRefusalChange: (js.Object, js.UndefOr[Int], String) => Unit = { (_, _, value) =>
             self.setState(_.copy(refusalReason = value))
           }
@@ -55,6 +55,32 @@ object FormRefuseProposalComponent {
                     self.setState(_.copy(errorMessage = Some("Oooops, something went wrong")))
                 }
             }
+
+          def handleNextProposal: SyntheticEvent => Unit = {
+            event =>
+              event.preventDefault()
+              val futureNextProposal =
+                for {
+                  _ <- ProposalService.refuseProposal(
+                    proposalId = self.props.wrapped.proposal.id,
+                    refusalReason = Option(self.state.refusalReason),
+                    notifyUser = self.state.notifyUser
+                  )
+                  nextProposal <- ProposalService
+                    .nexProposalToModerate(
+                      self.props.wrapped.proposal.operationId.toOption,
+                      self.props.wrapped.proposal.themeId.toOption,
+                      Some(self.props.wrapped.proposal.country),
+                      Some(self.props.wrapped.proposal.language)
+                    )
+                } yield nextProposal
+              futureNextProposal.onComplete {
+                case Success(proposalResponse) =>
+                  self.props.history.push(s"/nextProposal/${proposalResponse.data.id}")
+                case Failure(NotFoundHttpException) => self.props.history.push("/proposals")
+                case Failure(_)                     => self.setState(_.copy(errorMessage = Some("Oooops, something went wrong")))
+              }
+          }
 
           val selectReasons = <.SelectField(
             ^.floatingLabelText := "Refusal reason",
@@ -84,10 +110,15 @@ object FormRefuseProposalComponent {
                 ^.label := "Confirm refusal",
                 ^.onClick := handleSubmitRefuse
               )(),
+              <.RaisedButton(
+                ^.style := Map("float" -> "right"),
+                ^.label := "Next Proposal",
+                ^.onClick := handleNextProposal,
+                ^.disabled := self.state.isLocked
+              )(),
               errorMessage
             )
           )
-        }
-      )
+      })
     )
 }
