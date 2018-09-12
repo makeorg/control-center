cffb05a82ab157d382a5d5b33dd9f7658da3001a
cpestoury <cp@make.org>
2018-09-12 17:07:04 +0200

    feat(tag): change proposal by batch
    Closes #125

--- src/main/scala/org/make/backoffice/component/package.scala
+++ src/main/scala/org/make/backoffice/component/package.scala
@@ -44,7 +44,8 @@ package object component {
     def NewIdeaComponent: ReactClassElementSpec = self(proposal.common.NewIdeaComponent.reactClass)
     def IdeaTitle: ReactClassElementSpec = self(idea.EditIdea.ideaTitle)
     def TagTitle: ReactClassElementSpec = self(tag.EditTag.tagTitle)
-    def CustomDatagrid: ReactClassElementSpec = self(idea.EditIdea.dataGrid)
+    def CustomIdeaDatagrid: ReactClassElementSpec = self(idea.EditIdea.dataGrid)
+    def CustomTagDatagrid: ReactClassElementSpec = self(tag.EditTag.dataGrid)
     def StartModeration: ReactClassElementSpec = self(proposal.moderation.StartModeration.reactClass)
   }
 
